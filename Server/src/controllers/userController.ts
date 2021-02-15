875223bccf9ded3b594bfb920f3d4b2bd535566d
Jorge Salazar <201504215@ingenieria.usac.edu.gt>
2021-02-14 22:11:04 -0600

    verificacion login

--- Server/src/controllers/userController.ts
+++ Server/src/controllers/userController.ts
@@ -30,7 +30,7 @@ class UserController{
             return res.sendStatus(401);
         }
         const secretkey = 'f85e16c834b9e4d4c3c5f3c9bc7ce0d69ca633629d04d0f2a05c811a1c2686dcacc6b03cd4bad510956f610db15dcf2dafc3989182d479f8e9817078a0e4abb2';
-        jwt.sign({user}, secretkey, (err, token:any) => {
+        jwt.sign({user}, secretkey, (err:any, token:any) => {
             console.log("Se encontro el usuario");
             res.json({token});
         });
