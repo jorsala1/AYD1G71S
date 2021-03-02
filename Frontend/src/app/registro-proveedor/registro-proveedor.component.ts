73cf3afc87bd80693894ed011d484db9b42ed091
Marielos Herrera <3050583390117@ingenieria.usac.edu.gt>
2021-03-01 21:19:21 -0600

    Se agregó el service proveedor, se agregó la consulta para insertar proveedores. - 201504399

--- Frontend/src/app/registro-proveedor/registro-proveedor.component.ts
+++ Frontend/src/app/registro-proveedor/registro-proveedor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProveedorService } from '../services/proveedor.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro-proveedor',
@@ -7,12 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroProveedorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private proveedor:ProveedorService,public router: Router) { }
   nombre: string="";
   contacto: string="";
   telefono: string="";
   direccion: string="";
+  
   ngOnInit(): void {
   }
 
+  registrarse(){
+    // console.log(this.fechanac);
+     this.proveedor.RegistroProveedor(this.nombre,this.direccion,this.telefono,this.contacto)
+     .subscribe((res)=>{
+       console.log("ya insertó");
+       console.log(res);    
+       alert("PROVEEDOR REGISTRADO CON ÉXITO");
+       this.nombre="";
+       this.direccion="";
+       this.telefono=""; 
+       this.contacto="";
+     },
+     err => {alert("NO SE PUDO REGISTRAR EL PROVEEDOR")}  
+     )
+  
+   }
 }
