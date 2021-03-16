2fb072d81fac7bda281e52cd60308c2d48f0c0c0
Marielos Herrera <3050583390117@ingenieria.usac.edu.gt>
2021-03-16 03:02:24 -0600

    Se realizaron algunas pruebas unitarias al frontend  - 201504399

--- Frontend/src/app/components/ver-proveedores/ver-proveedores.component.spec.ts
+++ Frontend/src/app/components/ver-proveedores/ver-proveedores.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { VerProveedoresComponent } from './ver-proveedores.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('VerProveedoresComponent', () => {
   let component: VerProveedoresComponent;
@@ -8,7 +10,11 @@ describe('VerProveedoresComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VerProveedoresComponent ]
+      declarations: [ VerProveedoresComponent ],
+      imports: [
+        HttpClientTestingModule, RouterTestingModule.withRoutes([]),
+        ReactiveFormsModule, FormsModule
+      ]
     })
     .compileComponents();
   }));
--- Frontend/src/app/services/usuario.service.spec.ts
+++ Frontend/src/app/services/usuario.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UsuarioService } from './usuario.service';
 
 describe('UsuarioService', () => {
   let service: UsuarioService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule, RouterTestingModule.withRoutes([]),
+        ReactiveFormsModule, FormsModule
+      ]
+    });
     service = TestBed.inject(UsuarioService);
   });
 
