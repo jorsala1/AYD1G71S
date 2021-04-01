a6d09bfca018d1e5a26f92123c120d574556d3ec
Marielos Herrera <3050583390117@ingenieria.usac.edu.gt>
2021-03-31 21:14:12 -0600

    Agregar productos al carrito.  - 201504399

--- Frontend/src/app/components/productos-clientes/productos-clientes.component.ts
+++ Frontend/src/app/components/productos-clientes/productos-clientes.component.ts
@@ -11,6 +11,8 @@ export class ProductosClientesComponent implements OnInit {
 
   constructor(public router:Router, public Producto:ProductoService) { }
   Nombres: string;
+  Producto_Cantidad : number[][] =[]; //id producto, cantidad
+  ProductosElegidos : number []=[];
   VectorProductos: Producto[] =[];
   headElements = ['Nombre Producto', 'Descripción', 'Cantidad','Precio Venta','Precio Compra','Comprar'];
   habilitacion: string;
@@ -24,7 +26,7 @@ export class ProductosClientesComponent implements OnInit {
       this.habilitacion="<div class=\"dropdown-divider\"></div><a class=\"dropdown-item\" href=\"administrarUsuario\" >Admin Usuarios</a> <div class=\"dropdown-divider\"></div><a class=\"dropdown-item\" href=\"modificarUsuario\" >Modificar mis datos</a> <div class=\"dropdown-divider\"></div>    <a class=\"dropdown-item\"  href=\"verProveedores\" >Ver Proveedores</a> <div class=\"dropdown-divider\"></div>    <a class=\"dropdown-item\"  href=\"registroProveedor\" >Agregar Proveedor</a> <div class=\"dropdown-divider\"></div><a class=\"dropdown-item\" href=\"administrarProveedor\" >Admin Proveedor</a> <div class=\"dropdown-divider\"></div>    <a class=\"dropdown-item\"  href=\"agregarProducto\" >Agregar Producto</a>";
 
      }else{
-      this.habilitacion="<div class=\"dropdown-divider\"></div><a class=\"dropdown-item\" href=\"modificarUsuario\" >Modificar Datos</a>";
+      this.habilitacion="<div class=\"dropdown-divider\"></div><a class=\"dropdown-item\" href=\"modificarUsuario\" >Modificar Datos</a> ";
     }
 
     this.Producto.obtenerProductos().subscribe((res:any[])=>{
@@ -37,8 +39,35 @@ export class ProductosClientesComponent implements OnInit {
     localStorage.clear();
     this.router.navigate(['login']);
   }
-  AgregarCarrito(){
+  AgregarCarrito(id: number){
     this.contador++;
+    let cantidad =0;
+    localStorage.setItem("contadorp",this.contador+"");
+    if(this.VerificarCantidad(id)){
+      for (const p of this.Producto_Cantidad) {
+        if(p[0]==id){
+          cantidad= p[1];
+          cantidad++;
+          p[1]=cantidad;
+        }
+      }
+    }else {
+      this.ProductosElegidos.push(id);
+      this.Producto_Cantidad.push([id,1]);
+    }
+    console.log(this.Producto_Cantidad);
+  }
+  Comprar(){
+    
+  }
+
+  VerificarCantidad(id:number):boolean{
+    for (const prod of this.ProductosElegidos) {
+      if(prod == id ){      
+        return true;
+      }
+    }
+    return false;
   }
 
 }
