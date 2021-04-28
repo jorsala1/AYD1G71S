16a9b088b826d10ced36bda026659e49be2c8426
Marielos Herrera <3050583390117@ingenieria.usac.edu.gt>
2021-04-28 09:21:57 -0600

    Se agregaron pruebas 201504399

--- Frontend/src/app/components/admin-pedidos/admin-pedidos.component.spec.ts
+++ Frontend/src/app/components/admin-pedidos/admin-pedidos.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AdminPedidosComponent } from './admin-pedidos.component';
 
 describe('AdminPedidosComponent', () => {
@@ -8,7 +10,11 @@ describe('AdminPedidosComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminPedidosComponent ]
+      declarations: [ AdminPedidosComponent ],
+      imports: [
+        HttpClientTestingModule, RouterTestingModule.withRoutes([]),
+        ReactiveFormsModule, FormsModule
+      ]
     })
     .compileComponents();
   }));
--- Frontend/src/app/components/mis-pedidos/mis-pedidos.component.spec.ts
+++ Frontend/src/app/components/mis-pedidos/mis-pedidos.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MisPedidosComponent } from './mis-pedidos.component';
 
 describe('MisPedidosComponent', () => {
@@ -8,7 +10,11 @@ describe('MisPedidosComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MisPedidosComponent ]
+      declarations: [ MisPedidosComponent ],
+      imports: [
+        HttpClientTestingModule, RouterTestingModule.withRoutes([]),
+        ReactiveFormsModule, FormsModule
+      ]
     })
     .compileComponents();
   }));
